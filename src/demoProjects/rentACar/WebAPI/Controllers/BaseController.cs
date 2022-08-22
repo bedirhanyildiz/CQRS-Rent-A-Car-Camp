99dccb93db556dba578c5af04e0fb8a7813f666c
bedirhanyildiz <bedirhanyildiz94@gmail.com>
2022-08-22 23:48:23 +0300

    Business Rules

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class BaseController : Controller
+    {
+       
+        
+            protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+            private IMediator? _mediator;
+
+
+        
+    }
+}
